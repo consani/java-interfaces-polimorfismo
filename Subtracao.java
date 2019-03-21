523d0b63f54256c16611c61b5b5a21d7af1124dc
consani <braulio.consani@gmail.com>
2019-03-21 19:11:51 -0300

    Add files via upload

---
+++ Subtracao.java
@@ -0,0 +1,14 @@
+public class Subtracao implements Operacao{
+
+	@Override
+	public double executar(double operador1, double operador2) {
+		return operador1 - operador2;
+	}
+	
+	@Override
+	public String toString(){
+		return "Subtração";
+	}
+	
+
+}
